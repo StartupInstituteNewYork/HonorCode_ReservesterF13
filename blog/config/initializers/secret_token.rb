c1dfb13586a35670fdd9eef29d56fd329cb5a464
honor <katalina@voteseed.com>
2013-10-30 13:03:32 -0400

    blog created

--- blog/config/initializers/secret_token.rb
+++ blog/config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Blog::Application.config.secret_key_base = 'dea35b0a9d14b6ad56668260133f94f955afdc24af462808be898bf4f9deb4f020f3a2e170e31f20457a5ec3b66bb4e4903006269a76f1a8c073390765f08c47'
+Blog::Application.config.secret_key_base = '37238ef8517200370ee459c96ce5a5e81453345db0938530afbd3a96ead17c1767eb4f4d862d7f826a669f5c5db23f8c0f694f100eb139defdfeab6c698c4d0a'
