234ce354be5ff66766cde7ad6ffb2f5656f5bfbb
honor <katalina@voteseed.com>
2013-11-03 16:02:29 -0500

    rspec install

--- Reservester/config/initializers/secret_token.rb
+++ Reservester/config/initializers/secret_token.rb
@@ -9,4 +9,17 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Reservester::Application.config.secret_key_base = 'a727143a174e27d572d28527f32b2ebd0f9e63e485e382e69b94c83680c00354f870144af4bdb98828c0806745b5f423293a46e6a1cbb4555105f8b99010cfe3'
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Reservester::Application.config.secret_key_base = secure_token
